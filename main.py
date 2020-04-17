b6ada7c46bd51b52d75bd04bc5ebbf3f032361d3
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-04-18 03:13:26 +0900

    Added tree structure file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from image_to_csv import convert_image_to_csv
 from .make_csv_for_regression import make_csv_for_regression
 
 current_directory = os.getcwd()
-# file = convert_image_to_csv(current_directory, '13-11-19_mirpur', 'mirpurAllRoadSegments.csv')
+file = convert_image_to_csv(current_directory, '13-11-19_mirpur', 'mirpurAllRoadSegments.csv')
 
 file_for_regression = make_csv_for_regression(
     current_directory,
