32e88c04cd75efc9f91e0baa34cdfb7afd9b92ae
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-05-03 17:08:10 +0900

    Data filter using range

--- run_models.py
+++ run_models.py
@@ -19,12 +19,16 @@ data_set = '13-11-19_mirpurin_out.csv'
 data_path = os.path.join(current_directory, 'csvs', data_set)
 data = pd.read_csv(data_path).T
 
-# ha(data, pre_len=1, repeat=False, is_continuous=True)
+start_index = '13_11_2019 06_00_01_AM'
+end_index = '13_11_2019 08_01_31_AM'
+data = data.loc[start_index: end_index]
+
+ha(data, pre_len=1, repeat=False, is_continuous=True)
 # # ha_sampling(data, repeat=False, is_continuous=True, sampling_rate=3)
 #
 # svr(data, pre_len=1, repeat=False, is_continuous=True)
 
 # arima(data, pre_len=3, repeat=False, is_continuous=True)
 # arima_sampling(data, repeat=False, is_continuous=True, sampling_rate=3)
-
-sarima(data, rate=0.5, seq_len=12, pre_len=3, repeat=False, is_continuous=True)
+#
+# sarima(data, rate=0.5, seq_len=12, pre_len=3, repeat=False, is_continuous=True)
