c9f812fe8dbe5205e56d065831141eb83a21e64e
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-06-13 19:51:32 +0900

    added config file

---
+++ config.py
@@ -0,0 +1,21 @@
+config = {
+    'train': {
+        'set0': {
+            'start_date': '01_11_2019',
+            'end_date': '07_11_2019',
+            'start_time': '12_00_01',
+            'end_time': '18_00_01',
+        },
+        'set1': {
+            'start_date': '11_11_2019',
+            'end_date': '17_11_2019',
+        }
+    },
+    'test': {
+        'set0': {
+            'start_date': '08_11_2019',
+            'end_date': '11_11_2019',
+            'start_time': '19_00_01',
+        },
+    }
+}
