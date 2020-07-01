cd9ae596dad5b75733e3334977c6e150de985af3
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-07-02 00:26:42 +0900

    Added standard deviation condition

--- utils/process_per_segment.py
+++ utils/process_per_segment.py
@@ -1,8 +1,9 @@
 import numpy.linalg as la
 import math
+import statistics
 
 import numpy as np
-from sklearn.metrics import mean_squared_error,mean_absolute_error
+from sklearn.metrics import mean_squared_error,mean_absolute_error, r2_score
 import pandas as pd
 from scipy.stats.stats import pearsonr
 
@@ -14,18 +15,27 @@ def evaluation(a, b):
     mae = mean_absolute_error(a, b)
     a = a.flatten()
     b = b.flatten()
-    cor, _ = pearsonr(a, b)
-    return rmse, mae, cor
+    if not statistics.stdev(a) or not statistics.stdev(b):
+        cor = 1
+    else:
+        cor, _ = pearsonr(a, b)
+
+    if not statistics.stdev(a):
+        r2 = 1
+    else:
+        r2 = r2_score(a, b)
+
+    return rmse, mae, cor, r2
 
 
 def process_per_segment(whos, test, result):
-    answer =[[whos, whos, whos]]
-    answer.append(['RMSE', 'MAE', 'COR'])
+    answer =[[whos, whos, whos, whos]]
+    answer.append(['RMSE', 'MAE', 'COR', 'R2'])
 
     for i in range(len(test)):
-        rmse, mae, cor = evaluation(test[i], result[i])
-        answer.append([rmse, mae, cor])
-    rmse, mae, cor = evaluation(test, result)
-    answer.append([rmse, mae, cor])
+        rmse, mae, cor, r2 = evaluation(test[i], result[i])
+        answer.append([rmse, mae, cor, r2])
+    rmse, mae, cor, r2 = evaluation(test, result)
+    answer.append([rmse, mae, cor, r2])
 
     return answer
