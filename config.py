670c361e1c41eccff78997dbed4f5e6cf6f2deff
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-07-03 11:42:58 +0900

    testing

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ config = {
     'train': {
         'set0': {
             'start_date': '01_11_2019',
-            'end_date': '04_11_2019',
+            'end_date': '01_11_2019',
             'start_time': '12_34_01',
             'end_time': '18_36_31',
         },
@@ -10,7 +10,7 @@ config = {
     'test': {
         'set0': {
             'start_date': '01_11_2019',
-            'end_date': '04_11_2019',
+            'end_date': '01_11_2019',
             'start_time': '18_34_01',
             'end_time': '22_35_01',
         }
@@ -18,7 +18,7 @@ config = {
     'intensity_list_to_count': [3, 4],
     'is_intensity_continuous': True,
     'number_of_ignored_cell': 3,
-    'intermediate_file_save': True,
+    'intermediate_file_save': False,
     'sampling_rate': 2,
     'seq_len': 12,
     'pre_len': 10,
