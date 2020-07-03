f08db3b96cf58f32cebad28beca33c0c6e46c8ab
mahbubcseju <mahbuburrahman2111@gmail.com>
2020-07-03 11:51:51 +0900

    testing

--- utils/process_per_segment.py
+++ utils/process_per_segment.py
@@ -10,8 +10,9 @@ from scipy.stats.stats import pearsonr
 
 def evaluation(a, b):
     print(a, b)
-    a1 = np.array(a, dtype=np.float)
-    b1 = np.array(b, dtype=np.float)
+    # a1 = np.array(a, dtype=np.float)
+    # b1 = np.array(b, dtype=np.float)
+    a1, b1 = a, b
     rmse = math.sqrt(mean_squared_error(a1,b1))
     mae = mean_absolute_error(a1, b1)
     a1 = a1.flatten()
